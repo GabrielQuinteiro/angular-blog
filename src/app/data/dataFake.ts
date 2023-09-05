2b852738227fbbe93686fa28062966ebbb9aa066
Gabriel <gabrieldecb@gmail.com>
2023-09-05 15:02:53 -0300

    feeding the content with db

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+        "title": "[OPINIÃO] Homem de Ferro",
+        "description": "Porque o primeiro filme do Vingador mais carismático (e controverso), é considerado um dos melhores filmes já feitos da Marvel."
+    },
+    {
+        "id": "2",
+        "photo": "https://static.next-episode.net/tv-shows-images/huge/marvels-jessica-jones.jpg",
+        "title": "Jessica Jones: Kilgrave poderia ser salvo?",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta, odio eu tempor imperdiet, felis nibh facilisis metus, nec sollicitudin lorem lacus id libero. Sed id dignissim purus. In id sagittis ex. Vestibulum tincidunt dui quis auctor hendrerit. Cras non dapibus dui. Maecenas faucibus venenatis sollicitudin. Proin suscipit elit in sem porttitor, et mollis dui fringilla. Donec ligula eros, sodales sit amet convallis nec, placerat vitae erat. Nulla vulputate auctor ex quis ornare. Phasellus fringilla ex sit amet varius mollis. Phasellus dapibus leo non varius vulputate"
+    },
+    {
+        "id": "3",
+        "photo": "https://movies.universalpictures.com/media/06-opp-dm-mobile-banner-1080x745-now-pl-f01-071223-64bab982784c7-1.jpg",
+        "title": "Oppenheimer: Filme sobre o pai da bomba atômica",
+        "description": "Suspendisse sagittis mollis enim, ut lobortis risus. Nunc neque velit, dictum sed justo et, tempus vehicula arcu. Ut blandit enim at lectus lobortis, quis varius purus consequat. Maecenas posuere scelerisque ex. Aenean quis condimentum felis. Cras egestas eros sit amet neque dapibus, non vulputate felis aliquam. Proin facilisis fringilla quam, et congue est condimentum ut. Fusce feugiat purus vel velit sagittis, quis blandit diam sollicitudin. Cras sit amet orci tortor. In tempus suscipit elit sit amet placerat. Aliquam erat volutpat."
+    },
+    {
+        "id": "4",
+        "photo": "https://variety.com/wp-content/uploads/2023/08/Screen-Shot-2023-08-15-at-10.59.26-AM.png",
+        "title": "Cillian Murphy gostaria de ter estrelado Interstellar",
+        "description": "Integer pretium mi vel nibh pulvinar, eu ultricies arcu malesuada. Nam sed congue tellus, egestas ullamcorper lorem. Pellentesque auctor tortor vel lacinia molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce at ipsum erat. Pellentesque volutpat augue non tellus imperdiet luctus sit amet at ipsum. Donec condimentum est diam, at placerat turpis pulvinar sit amet. Maecenas rhoncus non nisl at finibus. Donec maximus dolor id malesuada feugiat. Morbi vel luctus orci. Maecenas auctor nisl eget dui pretium, a hendrerit dolor molestie. Vivamus et eros odio. Maecenas nec gravida purus. Fusce sagittis at ante nec vehicula. Nam ac elit quis nulla hendrerit tincidunt a eu diam. Donec varius sem ante, a suscipit urna accumsan vel"
+    }
+]
